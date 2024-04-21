d7a26623f3df9f33c41f6203e44574a653a1e3f1
João Victor <themaciel190@gmail.com>
2024-04-21 17:07:18 -0500

    initial structure of the react.js blog project

--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -1,10 +1,72 @@
 import React from "react";
+import logo from "../../svg/blog-logo.svg";
 
 const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <>
+      <header className="py-1 px-2">
+        <nav>
+          <div className="logo">
+            <a href=".">
+              <img src={logo} alt="Blog logo"></img>
+            </a>
+          </div>
+
+          <ul className="menu">
+            <li>
+              <a href="." className="p-1">
+                Categorias
+              </a>
+              <a href="." className="p-1">
+                Sobre
+              </a>
+              <a href="." className="p-1">
+                Contato
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="bx"></div>
+        <div className="flex-start-row">
+          <div className="search">
+            <form className="flex">
+              <input type="text" name="search" id="" placeholder="Buscar..." />
+              <button className="btn-search"></button>
+            </form>
+          </div>
+
+          <div className="cta-desktop ml-3">
+            <a href="." className="btn">
+              Login
+            </a>
+          </div>
+          <div className="cta-desktop ml-3">
+            <a href="." className="btn">
+              Login
+            </a>
+          </div>
+        </div>
+      </header>
+
+      <div className="relative">
+        <div className="menu-mobile">
+          <ul className="nav-mobile">
+            <li className="link-menu-mobile">
+              <a href=".">Categoria</a>
+            </li>
+            <li className="link-menu-mobile">
+              {" "}
+              <a href=".">SObre</a>
+            </li>
+            <li className="link-menu-mobile">
+              {" "}
+              <a href=".">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,32 @@
 import React from "react";
+import blog from "../../svg/blog.svg";
 
 const Home = () => {
   return (
-    <main>
-      <h1>Home</h1>
-    </main>
+    <>
+      <section className="container flex-center">
+        <div className="row mt-9">
+          <div className="grid-4">
+            <h1 className="h0">
+              Blog<span>.</span>{" "}
+            </h1>
+            <p>
+              Um blog para todos. Escrever. Ler. Comentar. Contribuir. Aprender.
+              Conectar
+            </p>
+            <a href="." className="btn mt-2">
+              Começar a escrever
+            </a>
+          </div>
+
+          <div className="grid-1"></div>
+          <div className="grid-6 flex-center">
+            <img src={blog} alt="" className="ilustration" />
+          </div>
+          <div className="grid-1"></div>
+        </div>
+      </section>
+    </>
   );
 };
 
